c1ed4c3994c8bf3016b53d393d5dcb5ae68b4c53
Chi Zhang <chizhang9135@icloud.com>
2023-03-08 20:05:25 -0800

    floydWarshall

---
+++ Floyd.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+// define the number of vertices in the graph
+#define V 4
+
+// function to perform the Floyd-Warshall algorithm
+void floydWarshall(int graph[V][V])
+{
+    int dist[V][V];
+
+    // initialize the distance matrix with the input graph
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // run the Floyd-Warshall algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // output the shortest path between all pairs of vertices
+    cout << "Shortest path between all pairs of vertices:" << endl;
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INT_MAX) {
+                cout << "INF ";
+            } else {
+                cout << dist[i][j] << "   ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int graph[V][V] = {
+        {0, 5, INT_MAX, 10},
+        {INT_MAX, 0, 3, INT_MAX},
+        {INT_MAX, INT_MAX, 0, 1},
+        {INT_MAX, INT_MAX, INT_MAX, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
